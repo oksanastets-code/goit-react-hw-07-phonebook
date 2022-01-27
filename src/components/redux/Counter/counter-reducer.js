0c8326a1568304b725e7926635d4300e7fa1baf4
Oksana Stets <oksanastets73@gmail.com>
2022-01-27 23:23:48 +0200

    add types

--- src/components/redux/Counter/counter-reducer.js
+++ src/components/redux/Counter/counter-reducer.js
@@ -1,17 +1,19 @@
 import { combineReducers } from 'redux';
+import { INCREMENT, DECREMENT } from './counter-types';
 
-const valueReducer = (state = 10, { type, payload }) => { 
-    switch (type) { 
-        case 'counter/Increment':
-            return state + payload;
-        case 'counter/Decrement':
-            return state - payload;
-        default:
-            return state;
-    }
-}
+const valueReducer = (state = 10, { type, payload }) => {
+  switch (type) {
+    case INCREMENT:
+      return state + payload;
+    case DECREMENT:
+      return state - payload;
+    default:
+      return state;
+  }
+};
 const stepReducer = (state = 5, action) => state;
-export default counterReducer = combineReducers({
-     value: valueReducer,
-        step: stepReducer, 
-})
+
+export default combineReducers({
+  value: valueReducer,
+  step: stepReducer,
+});
