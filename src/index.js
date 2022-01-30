08c848abc6038737bfc93e1da052647496bf964a
Oksana Stets <oksanastets73@gmail.com>
2022-01-30 22:41:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
-import store from 'components/redux/store';
+import { store, persistor } from 'components/redux/store';
 import  App from 'components/App';
 import 'modern-normalize/modern-normalize.css';
 import './styles.css';
@@ -10,8 +10,8 @@ import './styles.css';
 console.log(store);
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store.store}>
-        <PersistGate loading={null} persistor={store.persistor}>
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
       <App />
        </PersistGate>
     </Provider>
