8165fb1f0e684288a2fd4affd352049d2e52ed61
Oksana Stets <oksanastets73@gmail.com>
2022-02-09 22:38:10 +0200

    Update phone-book-operations.js

--- src/components/redux/PhoneBook/phone-book-operations.js
+++ src/components/redux/PhoneBook/phone-book-operations.js
@@ -5,14 +5,14 @@ axios.defaults.baseURL = 'https://6203af0f4d21c200170b9f5a.mockapi.io/api/v1/';
 
 export const addContact = ({ nick, number }) => dispatch => { 
   const contact = {
-    nick,
-    number,
+    nick: nick,
+    number: number,
   }
   
   dispatch(addContactRequest());
 
   axios.post('/contacts', contact)
-    .then(({ nick, number }) => dispatch(addContactSuccess(nick, number)))
+    .then(({ data }) => dispatch(addContactSuccess(data)))
     .catch(error => dispatch(addContactError(error)))
   // axios.get('/contacts').then(r => r.data)
 }
